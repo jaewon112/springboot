3f4b283a161af97f1e26880a979da4311c47262d
jaewon112 <ghfjf112@gmail.com>
2025-06-19 17:30:24 +0900

    250619

--- AuthStudy/src/main/java/com/korit/authstudy/controller/UserController.java
+++ AuthStudy/src/main/java/com/korit/authstudy/controller/UserController.java
@@ -1,10 +1,13 @@
 package com.korit.authstudy.controller;
 
+import com.korit.authstudy.dto.JwtDto;
 import com.korit.authstudy.dto.LoginDto;
 import com.korit.authstudy.dto.UserRegisterDto;
+import com.korit.authstudy.security.service.JwtService;
 import com.korit.authstudy.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UsersService usersService;
+    private final JwtService jwtService;
 
     @PostMapping
     public ResponseEntity<?> registerUser(@RequestBody UserRegisterDto dto) {
@@ -24,8 +28,16 @@ public class UserController {
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginDto dto) {
-        log.info("DTO: {}", dto);
-        usersService.login(dto);
-        return ResponseEntity.ok(usersService.login(dto));
+        JwtDto jwtDto = usersService.login(dto);
+        System.out.println("로그인 컨트롤러 호출");
+//        log.info("DTO: {}", dto);
+//        usersService.login(dto);
+        return ResponseEntity.ok(jwtDto);
+    }
+
+    @GetMapping("/login/status")
+    public ResponseEntity<?> getLoginStatus(@RequestHeader("Authorization") String authorization) {
+        System.out.println(authorization);
+        return ResponseEntity.ok(jwtService.validLoginAccessToken(authorization));
     }
 }
